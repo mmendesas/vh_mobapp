562b4499710feb6c955a4d34760df588dcc87792
Marcio Mendes <mmendes.as@gmail.com>
2020-04-20 22:49:47 -0300

    chore: initial commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, StatusBar} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.body}>
+        <Text>Hello world</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: Colors.lighter,
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
