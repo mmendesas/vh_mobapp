da9d3d848b7b343a30ea3a41ea63216bf23a058f
Marcio Mendes <mmendes.as@gmail.com>
2020-04-27 20:49:38 -0300

    feat: get data info in details page

--- src/pages/TabSearch/JobDetails/index.js
+++ src/pages/TabSearch/JobDetails/index.js
@@ -21,40 +21,41 @@ import Button from '~/components/Button';
 export default function JobDetails({ route }) {
   const { data } = route.params;
 
+  const {
+    company,
+    positionName,
+    salary,
+    city,
+    jobType,
+    deadline,
+    description,
+  } = data;
+
   return (
     <>
       <Container>
         <Company>
           <Image
-            source={{ uri: 'http://logo.clearbit.com/boldcommerce.com' }}
+            source={{
+              uri: `http://logo.clearbit.com/${company.toLowerCase()}.com`,
+            }}
           />
           <View>
-            <Position>Software Developer</Position>
-            <CompanyName>Bold Commerce</CompanyName>
+            <Position>{positionName}</Position>
+            <CompanyName>{company}</CompanyName>
           </View>
         </Company>
 
         <JobInfo>
-          <LabelText label="Location" text="Winnipeg, MB" />
-          <LabelText label="Salary" text="$60k - $70k (annually)" />
-          <LabelText label="Location" text="Full-time" />
-          <LabelText label="Deadline" text="April 23, 2020" />
+          <LabelText label="Location" text={city} />
+          <LabelText label="Salary" text={salary} />
+          <LabelText label="Job Type" text={jobType} />
+          <LabelText label="Deadline" text={deadline} />
         </JobInfo>
 
         <JobContent>
           <Title>Job Description</Title>
-          <Description>
-            Here's what we're looking for{'\n\n'}We’re looking for software
-            developers with a wide range of experience and exposure to different
-            technologies to add to our forward-thinking, driven development
-            teams. {'\n\n'}At Bold we use PHP, Go, Ruby, and JavaScript,
-            however, if your experience is with other languages we’d still love
-            to meet you! More than just technical skills, we’re looking for
-            like-minded individuals who are generally awesome and will make the
-            office a better place for everyone.{'\n\n'}We’re looking for someone
-            who: Likes to get shit done Thrives in an environment of talented,
-            hard-working people Resonates with our values
-          </Description>
+          <Description>{description.replace(/↵/g, '\n')}</Description>
         </JobContent>
       </Container>
       <Actions>
